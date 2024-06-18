8ca399661ef6e5587fcd7dc61372f8ba4dd5cda3
anitha2915 <anitha161203@gmail.com>
2024-06-18 11:14:47 +0530

    Add files via upload

--- orderController.js
+++ orderController.js
@@ -1,26 +1,57 @@
-const Order = require('../../../models/order');
+const Order = require('../../../models/order')
+const moment = require('moment')
+function orderController(){
+    return{
+        store(req,res){
+         //validate request
+        const {phone,address}=req.body
+         if(!phone||!address){
+           req.flash('error','All fields are required')
+           return res.redirect('/cart')
+         }
 
-function orderController() {
-  return {
-    async index(req, res) {
-      try {
-        const orders = await Order.find({ status: { $ne: 'completed' } })
-          .sort({ 'createdAt': -1 })
-          .populate('customerId', '-password')
-          .exec();
+         const order = new Order({
+            customerId:req.user._id,
+            items:req.session.cart.items,
+            phone,
+           address
+         })
 
-        if (req.xhr) {
-          return res.json(orders); // Return JSON response for AJAX request
-        } else {
-          return res.render('admin/orders', { orders }); // Render view for non-AJAX request
-        }
-      } catch (err) {
-        console.error('Error fetching orders:', err);
-        return res.status(500).send('An error occurred while fetching orders');
-      }
-    }
-  };
+         order.save().then(result =>{
+            req.flash('success','Order placed successfully')
+            delete req.session.cart
+            //Emit
+            const eventEmitter = req.app.get('eventEmitter');
+            eventEmitter.emit('orderUpdated', result);
+            return res.redirect('/customer/orders')
+
+         }).catch(err =>{
+            req.flash('error','Something went wrong')
+            return res.redirect('/cart')
+         })
+        },
+        async index(req,res){
+         const orders=await Order.find({ customerId:req.user._id},
+            null,
+            {sort:{ 'createdAt':-1}})
+            res.header('Cache-Control',' no-cache,no-store,must-revalidate,max-state=0 ,post-check=0,pre-check=0')
+            
+         res.render('customers/orders',{orders:orders,moment:moment})
+         console.log(orders)
+        },
+        async show(req, res) {
+        
+             const order = await Order.findById(req.params.id)
+             // Authorize user
+             if (req.user._id.toString() === order.customerId.toString()) {
+                  res.render('customers/singleOrder', { order })
+             } else {
+            
+             res.redirect('/customer/orders')
+         }
+     }
+   }
+    
 }
 
-module.exports = orderController;
-
+module.exports= orderController
